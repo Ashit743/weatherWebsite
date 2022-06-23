266b72e21886f9614d1566278c83b59adb7775d9
Ashit Darurmath <ashitsdarurmath@gmail.com>
2022-06-23 19:27:06 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit',(e)=>{
     message1.textContent = 'Enter a address!'
   }
   else{
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
       response.json().then((data)=>{
         if(data.error){
           console.log(data.error)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const express = require('express')
 const hbs = require('hbs')
+
+
 const app = express()
+const port = process.env.PORT || 3000
+
+
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
@@ -102,6 +107,6 @@ app.get('*',(req,res)=>{
 })
 
 //app.com
-app.listen(3000,()=>{
-  console.log('server is up on port 3000')
+app.listen(port,()=>{
+  console.log('server is up on port'+port)
 })  //http based website it is port 80, for local development port 3000 is good
